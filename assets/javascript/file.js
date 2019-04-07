6ab8e59c49dad70c158d7e494f28cfa006971110
Jason Moore <jason.moore1369@gmail.com>
2019-04-07 16:14:06 -0500

    bttn click event pushes newTrain obj into firebase

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -10,4 +10,51 @@ var config = {
     storageBucket: "",
     messagingSenderId: "712730153502"
 };
+
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainID = "";
+var destination = "";
+var firstArival = "";
+var frequency = "";
+
+$("#bttn").on("click", function(event) {
+    event.preventDefault();
+
+    trainID = $("#trainIdInput").val().trim();
+    destination = $("#destinationInput").val().trim();
+    firstArival = $("#firstTimeInput").val().trim();
+    frequency = $("#frequencyInput").val().trim();
+
+    
+    var newTrain = {
+        trainID: trainID,
+        destination: destination,
+        firstArival: firstArival,
+        frequency: frequency
+    };
+    
+    console.log("clicked");
+    console.log(newTrain);
+    
+    database.ref().push(newTrain);
+
+    $("#trainIdInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTimeInput").val("");
+    $("#frequencyInput").val("");
+    
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    
+})
+
+
+
+//var firstTime = moment(firstArival, "HH:mm").subtract(1, "years");
+//console.log(firstTime);
