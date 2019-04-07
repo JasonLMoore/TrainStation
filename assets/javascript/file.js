5fc941fc7cc7875116a97403827d0e8758fd7196
Jason Moore <jason.moore1369@gmail.com>
2019-04-07 14:44:55 -0500

    added styling and html

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
 });
+
+var config = {
+    apiKey: "AIzaSyDRmL8S2TUG5wh6VJ0410etBjAoO9xrgLU",
+    authDomain: "jlm-train-homework.firebaseapp.com",
+    databaseURL: "https://jlm-train-homework.firebaseio.com",
+    projectId: "jlm-train-homework",
+    storageBucket: "",
+    messagingSenderId: "712730153502"
+};
+firebase.initializeApp(config);
